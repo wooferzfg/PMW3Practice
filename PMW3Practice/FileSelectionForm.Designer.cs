f50bc75e654fcbc45d0214e8fb86fdf883e54226
Sergey Papushin <spapushin@gmail.com>
2018-03-31 01:42:10 -0500

    Initial commit

---
+++ PMW3Practice/FileSelectionForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace PMW3Practice
+{
+    partial class FileSelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbFile1 = new System.Windows.Forms.ComboBox();
+            this.cmbFile2 = new System.Windows.Forms.ComboBox();
+            this.cmbFile3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cmbFile1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cmbFile2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cmbFile3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 78);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "File 3:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "File 2:";
+            // 
+            // cmbFile1
+            // 
+            this.cmbFile1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFile1.FormattingEnabled = true;
+            this.cmbFile1.Location = new System.Drawing.Point(145, 3);
+            this.cmbFile1.Name = "cmbFile1";
+            this.cmbFile1.Size = new System.Drawing.Size(121, 21);
+            this.cmbFile1.TabIndex = 0;
+            this.cmbFile1.SelectedIndexChanged += new System.EventHandler(this.cmbFile1_SelectedIndexChanged);
+            // 
+            // cmbFile2
+            // 
+            this.cmbFile2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFile2.FormattingEnabled = true;
+            this.cmbFile2.Location = new System.Drawing.Point(145, 29);
+            this.cmbFile2.Name = "cmbFile2";
+            this.cmbFile2.Size = new System.Drawing.Size(121, 21);
+            this.cmbFile2.TabIndex = 1;
+            this.cmbFile2.SelectedIndexChanged += new System.EventHandler(this.cmbFile2_SelectedIndexChanged);
+            // 
+            // cmbFile3
+            // 
+            this.cmbFile3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFile3.FormattingEnabled = true;
+            this.cmbFile3.Location = new System.Drawing.Point(145, 55);
+            this.cmbFile3.Name = "cmbFile3";
+            this.cmbFile3.Size = new System.Drawing.Size(121, 21);
+            this.cmbFile3.TabIndex = 2;
+            this.cmbFile3.SelectedIndexChanged += new System.EventHandler(this.cmbFile3_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File 1:";
+            // 
+            // FileSelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 78);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FileSelectionForm";
+            this.Text = "PMW3 Practice Files";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbFile1;
+        private System.Windows.Forms.ComboBox cmbFile2;
+        private System.Windows.Forms.ComboBox cmbFile3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PMW3Practice/FileSelectionForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PMW3Practice
+{
+    public partial class FileSelectionForm : Form
+    {
+        string[] files = new string[] { "CliffsFight4Skip", "ValeRingsSkip", "ZephyrSyphon",
+            "CatacombsDetonator", "CatacombsEnd", "GogekkaHeightsTriplePillarless",
+            "GogekkaHeightsEnd", "CanyonEarly", "ErwinsFightSkip",
+            "DungenFight", "ZenithEarly", "ZenithLate" };
+
+        string appDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string gameDirectory = @"Electronic Arts\Pac-Man World 3";
+        string savesDirectory = @"D:\PMW3\PMW3Practice\PracticeFiles";
+
+        public FileSelectionForm()
+        {
+            InitializeComponent();
+
+            cmbFile1.Items.AddRange(files);
+            cmbFile2.Items.AddRange(files);
+            cmbFile3.Items.AddRange(files);
+        }
+
+        private void cmbFile1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            copyFile((string)cmbFile1.SelectedItem, "A");
+        }
+
+        private void cmbFile2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            copyFile((string)cmbFile1.SelectedItem, "B");
+        }
+
+        private void cmbFile3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            copyFile((string)cmbFile1.SelectedItem, "C");
+        }
+
+        private void copyFile(string fileName, string slot)
+        {
+            var sourcePath = Path.Combine(savesDirectory, fileName);
+            var destPath = Path.Combine(appDataDirectory, gameDirectory, "PMW3" + slot);
+            File.Copy(sourcePath, destPath, true);
+        }
+    }
+}
